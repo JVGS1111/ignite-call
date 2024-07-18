c8794b535ce592580ec1b43802d8ef1fb64a718e
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-07-18 12:00:48 -0300

    implemented cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,24 +1,48 @@
 import { prisma } from '@/lib/prisma'
+import { setCookie } from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type User = {
   name: string
   username: string
 }
+
+type Error = {
+  message: string
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<User>,
+  res: NextApiResponse<User | Error>,
 ) {
   if (req.method !== 'POST') {
     return res.status(405).end()
   }
 
   const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already exists',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 4 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
